2a3d21de0c1820d3edb65c39f3d2daac0f13f584
Andrea Fonti <andreafontict@gmail.com>
2011-04-07 18:59:55 +0200

    sistemato rspec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,14 +3,18 @@ require 'spec_helper'
 describe HomeController do
 	
 	describe "GET 'index'" do
+		render_views
+		
 		it "should be successful" do
 			get 'index'
 			response.should be_success
 		end
 		
-		it "Deve esserci il testo 'benvenuti nella nostra home page' " do
-			get :index
-			response.should have_selector("div",:id=>"background" ,:contain=> "benvenuti")
+		it "Deve esserci il testo 'benvenuti nella nostra home pa ge' " do
+			get 'index'
+			response.body.should have_selector("div",:id=>"background")  do |div|
+				div.should contain("benvenuti")
+			end
 		end
 	end
 	
