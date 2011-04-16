0378995c97ef0c875d3b8dd376a588760d51bf19
Andrea Fonti <andreafontict@gmail.com>
2011-04-16 15:26:59 +0200

    l'utente può ora loggarsi anche attraverso l'username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,17 @@ class User < ActiveRecord::Base
 	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>"}
-	 	# Setup accessible (or protected) attributes for your model
-	attr_accessible :email, :password, :password_confirmation, :remember_me,:is_admin,:avatar
+	# Setup accessible (or protected) attributes for your model
+	attr_accessible :email, :password, :password_confirmation, :remember_me,:is_admin,:avatar,:user_name,:login
+	#crea un campo che non viene scritto su database ma funge da intermediario per abilitare l'autenticazione sia con username che con email'
+	attr_accessor :login
+
+
+	#questo metodo serve per permettere a devise di autenticare l'utente fornendo o username o email'
+	protected
+	def self.find_for_database_authentication(warden_conditions)
+		conditions = warden_conditions.dup
+		login = conditions.delete(:login)
+		where(conditions).where(["user_name = :value OR email = :value", { :value => login }]).first
+	end
 end
