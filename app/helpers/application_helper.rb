600be944b2bf4aa6af72f071c46b16d773a74056
Andrea Fonti <andreafontict@gmail.com>
2011-05-22 20:02:49 +0200

    clean

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-	def default_editable_field(object, field,options={})
-		editable_field object, field,	{:submitdata => {:remote=>true},
+  def default_editable_field(object, field,options={})
+    editable_field object, field,	{:submitdata => {:remote=>true},
 										#:cancel     => 'Cancel',
 										#:submit     => 'OK',
 										:indicator  => 'Loading',
@@ -8,5 +8,20 @@ module ApplicationHelper
 										:cssclass	=> 'jeditable_form',
 										#:style		=> "display:inline",
 										:width		=> "200"}.merge(options)
-	end
+  end
+  
+  def breadcrumb_link_to(path,text=nil)
+    
+  content_tag "li" do    
+    l=link_to path do
+      text unless text.nil?
+      yield if block_given?
+    end
+    
+    a=image_tag 'arrow.png', width: 30
+    
+    l+a
+  end 
+  
+  end
 end
